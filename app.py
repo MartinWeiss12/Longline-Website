67c280dede46436270a3f9d54c49ae53e60124bd
Martin Weiss <martinweiss1255@gmail.com>
2023-03-14 20:10:04 -0400

    Added IAM role

--- app.py
+++ app.py
@@ -15,13 +15,12 @@ from botocore.exceptions import NoCredentialsError
 
 app = Flask(__name__, template_folder='templates')
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+#AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+#AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+#s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-print(AWS_ACCESS_KEY_ID)
-print(AWS_SECRET_ACCESS_KEY)
-
+session = boto3.Session()
+s3 = session.client('s3')
 
 flaskBackendPin = '1234'
 
